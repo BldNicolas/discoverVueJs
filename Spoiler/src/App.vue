90182b54290ad82c93ededbf22006fc467d28ff7
Nicolas <ni.berlaud@gmail.com>
2024-09-11 17:21:27 +0200

    🎉 Init : spoiler

---
+++ Spoiler/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Coucou</h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+        expedita temporibus nesciunt possimus ducimus quia sequi molestiae,
+        autem labore,
+        <SpoilerAlert>Coucou je suis une mega spoil</SpoilerAlert>
+        culpa explicabo? Aspernatur at laudantium cupiditate libero distinctio,
+        nihil rem aliquam!
+    </div>
+</template>
+
+<script>
+import SpoilerAlert from "./components/SpoilerAlert.vue";
+
+export default {
+    components: {
+        SpoilerAlert,
+    },
+};
+</script>
---
+++ Spoiler/src/components/SpoilerAlert.vue
@@ -0,0 +1,34 @@
+<template>
+    <span class="spoiler">
+        <span v-show="showSpoil">
+            <slot />
+        </span>
+        <span v-show="!showSpoil" @click="showSpoil = true">
+            Attention spoil
+        </span>
+    </span>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showSpoil: false,
+        };
+    },
+    methods: {
+        showSpoilFct() {
+            this.showSpoil = true;
+        },
+    },
+};
+</script>
+
+<style>
+.spoiler {
+    cursor: pointer;
+    background-color: red;
+    color: white;
+    width: fit-content;
+}
+</style>
