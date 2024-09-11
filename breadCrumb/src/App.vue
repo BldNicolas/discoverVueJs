29a2294244aaf7d2e34f54be8fc5a25d7126289e
Nicolas <ni.berlaud@gmail.com>
2024-09-11 17:20:00 +0200

    ✨ Init : breadcrumb

---
+++ breadCrumb/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <MyBreadCrumb
+        :pages="{
+            parents: [
+                {
+                    name: 'Accueil',
+                    path: '/',
+                },
+                {
+                    name: 'Formations',
+                    path: '/formations',
+                },
+                {
+                    name: 'Étudiant',
+                    path: '/formations/etudient',
+                },
+            ],
+            active: {
+                name: 'HTML',
+            },
+        }"
+        separator=">"
+    />
+</template>
+
+<script>
+import MyBreadCrumb from "./components/MyBreadCrumb.vue";
+
+export default {
+    components: {
+        MyBreadCrumb,
+    },
+};
+</script>
---
+++ breadCrumb/src/components/MyBreadCrumb.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <span v-for="parent in pages.parents">
+            <a :href="parent.path">{{ parent.name }}</a>
+            {{ this.separator }}
+        </span>
+        <span>{{ pages.active.name }}</span>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        pages: Object,
+        separator: String,
+    },
+};
+</script>
+
+<style>
+nav, nav * {
+    display: flex;
+    gap: 1rem;
+    justify-content: end;
+    background-color: blue;
+    color: white;
+}
+
+a {
+    text-decoration: none !important;
+}
+</style>
