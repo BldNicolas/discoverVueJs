63284cc525e07cb2f2b44a67ba85532a36a119a0
Nicolas <ni.berlaud@gmail.com>
2024-09-13 16:21:40 +0200

    🎉 Init : Alert

---
+++ Alert/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <div class="alerts">
+      <SuperAlert type="success">allerte succès</SuperAlert>
+      <SuperAlert type="danger">allerte danger</SuperAlert>
+      <SuperAlert type="error">allerte error</SuperAlert>
+    </div>
+  </div>
+</template>
+
+<script>
+import SuperAlert from '@/components/SuperAlert.vue';
+
+export default {
+  components: {
+    SuperAlert
+  },
+};
+</script>
+
+<style scoped>
+.alerts {
+  display: flex;
+  gap: 1rem;
+}
+</style>
---
+++ Alert/src/components/SuperAlert.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="alert" :class="`alert-${type}`">
+        <slot></slot>
+        <i>X</i>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: String
+        }
+    }
+</script>
+
+<style scoped>
+    .alert {
+        background-color: #ddd;
+        padding: 1rem;
+        border-radius: 0.375rem;
+    }
+
+    .alert-danger {
+        background-color: orange;
+    }
+
+    .alert-error {
+        background-color: red;
+    }
+
+    .alert-success {
+        background-color: green;
+    }
+</style>
